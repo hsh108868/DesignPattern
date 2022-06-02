77a1ff572866ffc608f7f3332ce393766a4052de
hsh108868 <pusrty@gmail.com>
2022-06-03 02:18:47 +0900

    observer

---
+++ Observer/App.java
@@ -0,0 +1,21 @@
+package Observer;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class App {
+    public static void main(String[] args) {
+        Button button = new Button();
+
+        button.addObserver(new Observer() {
+            @Override
+            public void update(Observable o, Object arg) {
+                System.out.println(o + " is Clicked");
+            }
+        });
+
+        button.onClick();
+        button.onClick();
+        button.onClick();
+    }
+}
---
+++ Observer/Button.java
@@ -0,0 +1,10 @@
+package Observer;
+
+import java.util.Observable;
+
+public class Button extends Observable {
+    public void onClick() {
+        setChanged();
+        notifyObservers();
+    }
+}
